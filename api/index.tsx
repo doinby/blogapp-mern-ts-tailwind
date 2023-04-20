c8a1b007006f5d9622f8a39c940af09f1ca2de7a
Chinh Do <chinh@doinby.co>
2023-04-20 23:49:03 +0700

    Add function to register with express

--- api/index.tsx
+++ api/index.tsx
@@ -1,8 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 
 const PORT = 4000;
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.use(cors());
+app.use(express.json());
+
+app.post('/register', (req, res) => {
+	const { username, password } = req.body;
+	res.json({ reqData: { username, password } });
+});
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,17 +1,39 @@
+import { useState } from 'react';
+
 export default function Register() {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+
+	const register = async (e: React.SyntheticEvent) => {
+		e.preventDefault();
+		try {
+			await fetch('http://localhost:4000/register', {
+				method: 'POST',
+				body: JSON.stringify({ username, password }),
+				headers: { 'Content-Type': 'application/json' },
+			});
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
 	return (
 		<main className='container mx-auto'>
 			<form
-				action=''
+				onSubmit={register}
 				className='h-full flex flex-col gap-6 justify-center items-center'>
 				<input
 					type='text'
 					placeholder='username'
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
 					className='px-4 py-2 border border-indigo-500 rounded-xl'
 				/>
 				<input
 					type='password'
 					placeholder='password'
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
 					className='px-4 py-2 border border-indigo-500 rounded-xl'
 				/>
 				<button className='bg-indigo-500 text-white rounded-xl px-4 py-2'>
