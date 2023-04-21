46f34490eec02305097f93c0db2342c793f53cc5
Chinh Do <chinh@doinby.co>
2023-04-21 12:17:45 +0700

    Add function to encrypt password using bcrypt

--- api/index.tsx
+++ api/index.tsx
@@ -2,11 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import * as dotenv from 'dotenv';
 import connectDB from './db';
+import bcrypt from 'bcrypt';
 
 // Models
 import { User } from './models/User';
 
 const PORT = 4000;
+const saltRounds = 10;
+
 const app = express();
 
 dotenv.config({ path: './api/.env' });
@@ -16,9 +19,13 @@ app.use(cors());
 app.use(express.json());
 
 app.post('/register', async (req, res) => {
-	const { username, password } = req.body;
+	const { username, password: plainPass } = req.body;
+
+	// Create hashed password before storing in DB
+	const hashedPass = await bcrypt.hash(plainPass, saltRounds);
+
 	try {
-		const userInfo = await User.create({ username, password });
+		const userInfo = await User.create({ username, password: hashedPass });
 		res.json(userInfo);
 	} catch (err) {
 		res.status(400).json(err.message);
