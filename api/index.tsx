948a2551fad60bd85bad8be6781524f42d35209e
Chinh Do <chinh@doinby.co>
2023-04-22 09:49:46 +0700

    Refactor bcrypt function and error handling

--- api/index.tsx
+++ api/index.tsx
@@ -21,12 +21,26 @@ app.use(express.json());
 app.post('/register', async (req, res) => {
 	const { username, password: plainPass } = req.body;
 
-	// Create hashed password before storing in DB
+	// Create hashed password to store in DB
 	const hashedPass = await bcrypt.hash(plainPass, saltRounds);
 
 	try {
 		const userInfo = await User.create({ username, password: hashedPass });
 		res.json(userInfo);
+	} catch (err) {
+		res.status(400).json(err.message);
+	}
+});
+
+app.post('/login', async (req, res) => {
+	const { username, password } = req.body;
+
+	try {
+		const userInfo = await User.findOne({ username });
+		const match = await bcrypt.compare(password, userInfo?.password);
+		match
+			? res.json('Login Matched!')
+			: res.status(400).json('Login Not Matched!');
 	} catch (err) {
 		res.status(400).json(err.message);
 	}
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,22 +1,47 @@
+import { useState } from 'react';
+
 export default function Login() {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [message, setMessage] = useState('placeholder');
+	const [msgColor, setMsgColor] = useState('');
+
+	const hasMsg = message !== 'placeholder' ? 'visible' : 'invisible';
+
+	const register = async (e: React.SyntheticEvent) => {
+		e.preventDefault();
+		const res = await fetch('http://localhost:4000/login', {
+			method: 'POST',
+			body: JSON.stringify({ username, password }),
+			headers: { 'Content-Type': 'application/json' },
+		});
+		setMessage(res.statusText);
+
+		res.ok ? setMsgColor('text-green-500') : setMsgColor('text-red-500');
+	};
+
 	return (
-		<main className='container mx-auto'>
-			<form
-				action=''
-				className='h-full flex flex-col gap-6 justify-center items-center'>
+		<main className='container mx-auto flex flex-col gap-10 place-items-center place-content-center'>
+			<h2 className='text-indigo-500 text-2xl'>Login</h2>
+			<form onSubmit={register} className='grid gap-6'>
 				<input
 					type='text'
 					placeholder='username'
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
 					className='px-4 py-2 border border-indigo-500 rounded-xl'
 				/>
 				<input
 					type='password'
 					placeholder='password'
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
 					className='px-4 py-2 border border-indigo-500 rounded-xl'
 				/>
 				<button className='bg-indigo-500 text-white rounded-xl px-4 py-2'>
-					Login
+					Submit
 				</button>
+				<p className={`${msgColor} ${hasMsg} text-center`}>{message}</p>
 			</form>
 		</main>
 	);
