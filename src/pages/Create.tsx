3f8308c53a3bf42a6abcb37d173403aeca2b6852
Chinh Do <chinh@doinby.co>
2023-05-08 12:54:27 +0700

    Add Quill text editor to Create form

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -1,12 +1,64 @@
+import { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+const modules = {
+	toolbar: [
+		[{ header: [1, 2, false] }],
+		['bold', 'italic', 'underline', 'strike', 'blockquote'],
+		[{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
+		['link', 'image'],
+		['clean'],
+	],
+};
+
+const formats = [
+	'header',
+	'bold',
+	'italic',
+	'underline',
+	'strike',
+	'blockquote',
+	'list',
+	'bullet',
+	'indent',
+	'link',
+	'image',
+];
+
 export default function Create() {
+	const [title, setTitle] = useState<string>('');
+	const [desc, setDesc] = useState<string>('');
+	const [content, setContent] = useState<string>('');
+
 	return (
 		<main className='container mx-auto flex flex-col gap-10 place-items-center place-content-center'>
 			<h2 className='text-indigo-500 text-2xl'>Create Post</h2>
-			<form className='grid gap-6'>
-				<input type='title' placeholder='Title' />
-				<input type='description' placeholder='Description' />
+			<form className='flex flex-col gap-6'>
+				<input
+					type='title'
+					placeholder='Title...'
+					value={title}
+					onChange={(e) => setTitle(e.target.value)}
+				/>
+				<input
+					type='description'
+					placeholder='Description...'
+					value={desc}
+					onChange={(e) => setDesc(e.target.value)}
+				/>
 				<input type='file' />
-				<textarea name='' id='' cols='30'></textarea>
+				<ReactQuill
+					className='h-[150px]'
+					theme='snow'
+					modules={modules}
+					formats={formats}
+					value={content}
+					onChange={setContent}></ReactQuill>
+				{/* <p className={`${msgColor} ${hasMsg} text-center`}>{message}</p> */}
+				<button className='bg-indigo-500 text-white rounded-xl px-4 py-2'>
+					Submit
+				</button>
 			</form>
 		</main>
 	);
